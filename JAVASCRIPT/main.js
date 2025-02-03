22a9e71d659011383ade5e7d4df18f314f2f338d
PATRICK MUJUNG PITIA NYARAKOSUK <mujungpatrick88@gmail.com>
2025-02-03 18:08:23 +0300

    Add files via upload

---
+++ JAVASCRIPT/main.js
@@ -0,0 +1,17 @@
+let name ="joan";
+console.log(name);
+
+let age = 20;
+let isStudent= true;
+let isOnline= false;
+let isMale = false;
+
+const price = 3000;
+
+let v = null;
+
+let person ={
+    name:'john',
+    grade:"1st class",
+    age:20,
+}
